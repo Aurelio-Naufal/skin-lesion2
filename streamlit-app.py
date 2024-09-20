38a8dcb6068ac05d7cc6ce9bfd76877ef6b51292
Aurelio-Naufal <113653626+Aurelio-Naufal@users.noreply.github.com>
2024-09-20 19:54:17 +0700

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -6,7 +6,7 @@ import cv2
 from torchvision import transforms
 import numpy as np
 import streamlit as st
-from PIL import Image
+import Image
 
 st.set_option('deprecation.showfileUploaderEncoding',False)
 st.title("Pengklasifikasi Lesi Kulit")
